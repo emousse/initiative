435d9154de059139b9a11fa71b1386f38197e99e
emousse <etienne.mousse@gmail.com>
2017-04-28 17:20:44 +0200

    daily update

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,26 @@
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+});
+
+var className = "is-active";
+var elem = document.querySelector('#menu--main');
+var content = document.querySelector('#dropdown-content');
+elem.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (content.classList) {
+        content.classList.toggle(className);
+    } else {
+        var classes = content.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0)
+            classes.splice(existingIndex, 1);
+        else
+            classes.push(className);
+
+        content.className = classes.join(' ');
+    }
 });
